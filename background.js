42af542df5e4a3966cc49ad4512f168d7597230f
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-06-01 19:59:35 +0300

    update background to get value from storage

--- background.js
+++ background.js
@@ -1,16 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.alarms.create('waterReminder', { periodInMinutes: 1 });
-  });
-  
-  chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.storage.sync.get(['reminderInterval'], function(result) {
+        let reminderInterval = result.reminderInterval;
+        if (reminderInterval === undefined) {
+            reminderInterval = 15; 
+            chrome.storage.sync.set({ reminderInterval: 15 });
+        }
+        chrome.alarms.create('waterReminder', { periodInMinutes: reminderInterval });
+    });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === 'waterReminder') {
-      chrome.notifications.create({
-        type: 'basic',
-        iconUrl: "icons/water-bottle.png",
-        title: 'Hydration Reminder',
-        message: 'Stay hydrated! It\'s time to drink some water.',
-        priority: 2
-      });
+        chrome.notifications.create({
+            type: 'basic',
+            iconUrl: "icons/water-bottle.png",
+            title: 'Hydration Reminder',
+            message: 'Stay hydrated! It\'s time to drink some water.',
+            priority: 2
+        });
     }
-  });
-  
+});

ea0a4fc502445806774e1fe791d044edfe35a535
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-06-03 09:44:18 +0300

    fix options page path

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.querySelector('#go-to-options').addEventListener('click', function() {
     if (chrome.runtime.openOptionsPage) {
       chrome.runtime.openOptionsPage();
     } else {
-      window.open(chrome.runtime.getURL('options.html'));
+      window.open(chrome.runtime.getURL('options/index.html'));
     }
   });
   
